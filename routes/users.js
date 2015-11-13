a4306e3a192c0a20d5b7aa05b9b1a00da6e8ae01
shichiku_yuki <shichiku_yuki@cyberagent.co.jp>
2015-11-13 12:58:24 +0900

    user page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('../lib/mongo.js');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:userId', function(req, res, next) {
+  var userId = req.params.userId;
+  mongo.find('user', {userId: userId}, {userId: true, userPublicScore: true, userImage: true}, function(result){
+    console.log(result);
+    res.render('user', {user: result[0]});
+  });
 });
 
 module.exports = router;
