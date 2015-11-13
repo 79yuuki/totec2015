42825e6fe99f6f667795a1418476be6bce3d0fde
shichiku_yuki <shichiku_yuki@cyberagent.co.jp>
2015-11-13 13:33:19 +0900

    Added mongo server ip

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var items = require('./routes/items');
+var posts = require('./routes/posts');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/user', users);
 app.use('/item', items);
+app.use('/post', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
