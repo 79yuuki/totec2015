2011b7b2d66ed0d342acbb9fc40d6db06c0b8e1e
shichiku_yuki <shichiku_yuki@cyberagent.co.jp>
2015-11-13 18:10:10 +0900

    temp

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,29 @@ var express = require('express');
 var router = express.Router();
 var mongo = require('../lib/mongo.js');
 var async = require('neo-async');
-var searchItem = require('./searchItem');
+var _ = require('lodash');
+
+function si(userId, callback) {
+  var limit = 8;
+  mongo.find('post', {postUserId: userId }, {}, limit, function(results){
+    var parallels = [];
+
+    // 8件のデータから画像パスとる
+    var itemIds = [];
+    results.forEach(function(result){
+      itemIds.push(result.postItemId);
+    });
+
+    mongo.find('item', {itemId: {$in: itemIds}}, {}, 8, function(itemres){
+      var i = [];
+      itemres.forEach(function(item){
+        i.push(item.itemImage);
+      });
+      return callback(i);
+    });
+
+  });
+};
 
 /* GET users listing. */
 router.get('/:userId', function(req, res, next) {
@@ -11,13 +33,25 @@ router.get('/:userId', function(req, res, next) {
     // console.log(result);
     var r = result[0];
     var parallels = [];
-    var user = {
-      userId: r.userId,
-      userPublicScore: r.userPublicScore,
-      userImage: r.userImage,
-      userFriends: r.userFriends.split(',')
-    };
-    res.render('user', {user: user});
+    parallels.push(
+      function(done) {
+        si(userId, done);
+      }
+    );
+
+    // TODO sp
+
+    async.parallel(parallels, function(result){
+
+      var user = {
+        userId: r.userId,
+        userPublicScore: r.userPublicScore,
+        userImage: r.userImage,
+        userFriends: r.userFriends.split(',')
+      };
+console.log('search============', result);
+      res.render('user', {user: user, searchItem: result});
+    });
   });
 });
 
