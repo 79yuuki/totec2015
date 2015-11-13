aa86d185fcdf7c7fcf6ffe55c13620a623a57df4
Katsutoshi Nagaoka <nagaoka_katsutoshi@cyberagent.co.jp>
2015-11-13 17:18:39 +0900

    aa

--- ansible/scripts/release.sh
+++ ansible/scripts/release.sh
@@ -5,6 +5,7 @@ export PATH=${PATH}:/usr/local/bin
 cd /opt/totec/totec2015
 git pull
 npm install .
+rm -rf /opt/nginx/proxy_cache/*
 service nginx restart
 /usr/local/bin/pm2 restart ./process.json
 
