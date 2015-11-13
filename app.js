4783960ae535b483db2cf9843b6396ee5c4b84c2
Katsutoshi Nagaoka <nagaoka_katsutoshi@cyberagent.co.jp>
2015-11-13 16:39:25 +0900

    add

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var items = require('./routes/items');
 var posts = require('./routes/posts');
 
 var searchUser = require('./routes/searchUser');
+var searchItem = require('./routes/searchItem');
 
 var app = express();
 
@@ -32,6 +33,7 @@ app.use('/item', items);
 app.use('/post', posts);
 
 app.use('/searchUser', searchUser);
+app.use('/searchItem', searchItem);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/searchItem.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('../lib/mongo.js');
+var _ = require('lodash');
+
+/* GET items listing. */
+router.get('/', function(req, res, next) {
+  var query = req.query;
+  // test1
+  var findByItemId = query.findByItemId;
+  var findByItemSupplier = query.findByItemSupplier;
+  var findByItemSoldQuantityGTE = query.findByItemSoldQuantityGTE;
+  var findByItemSoldQuantityLTE = query.findByItemSoldQuantityLTE;
+  var findByItemSalePriceGTE = query.findByItemSalePriceGTE;
+  var findByItemSalePriceLTE = query.findByItemSalePriceLTE;
+  var findByItemTagsIncludeAll = query.findByItemTagsIncludeAll;
+  var findByItemTagsIncludeAny = query.findByItemTagsIncludeAny;
+
+  var mongoQuery = {};
+  
+  if (findByItemId) {
+    _.assign(mongoQuery, {itemId: findByItemId});
+  }
+
+  if (findByItemSupplier) {
+    _.assign(mongoQuery, {itemNo: findByItemSupplier});
+  }
+
+  if (findByItemSoldQuantityGTE) {
+    _.assign(mongoQuery, {itemSoldQuantity: {$gte: Number(findByItemSoldQuantityGTE)}});
+  }
+
+  if (findByItemSoldQuantityLTE) {
+    _.assign(mongoQuery, {itemSoldQuantity: {$lte: Number(findByItemSoldQuantityLTE)}});
+  }
+
+  if (findByItemSalePriceGTE) {
+    _.assign(mongoQuery, {itemSalePrice: {$gte: Number(findByItemSalePriceGTE)}});
+  }
+
+  if (findByItemSalePriceLTE) {
+    _.assign(mongoQuery, {itemSalePrice: {$lte: Number(findByItemSalePriceLTE)}});
+  }
+
+  // TODO: findByItemTagsIncludeAll
+  // TODO: findByItemTagsIncludeAny
+
+  var limit = query.limit;
+console.log('mongo query ===', mongoQuery);
+  mongo.find('item', mongoQuery, {}, limit, function(result){
+    // console.log(result);
+    _.map(result, function(r){
+      r.itemTags = r.itemTags.split(',')
+    });
+
+    res.json({result: true, data: result});
+  });
+});
+
+module.exports = router;
